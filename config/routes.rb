be3780541cec9c4aa9e341a18b85d77b34538128
Chad Sakonchick <chadwicksakonchick@gmail.com>
2013-02-10 16:14:58 -0600

    Reorganized routes in nav to

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Appcenter::Application.routes.draw do
+  resources :items
+
+
   devise_for :users
 
   get 'about' => 'pages#about'
 
-  root :to => 'pages#home'
+  get 'home' => 'pages#home'
+
+  root :to => 'items#index'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
