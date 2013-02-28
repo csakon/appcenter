a75aaf2e3e2b854b422bc5e51c8f76ff51f4901d
Chad Sakonchick <chadwicksakonchick@gmail.com>
2013-02-27 20:43:54 -0600

    Maybe Oregon S3 now?

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,7 +75,7 @@ Appcenter::Application.configure do
     :bucket => ENV['AWS_BUCKET'],
     :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
     :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-    :s3_host_name => 's3-eu-west-2.amazonaws.com'
+    :s3_host_name => ENV['s3-eu-west-2.amazonaws.com']
   }
 }
 end
