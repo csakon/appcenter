539c543fd7a0c3989d4d917b0cf8a60d993d6a19
Chad Sakonchick <chadwicksakonchick@gmail.com>
2013-02-27 21:21:32 -0600

    Add jQuery Masonry

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,7 +4,7 @@ class ItemsController < ApplicationController
   # GET /items
   # GET /items.json
   def index
-    @items = Item.all
+    @items = Item.order("created_at desc")
 
     respond_to do |format|
       format.html # index.html.erb
